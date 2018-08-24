2fe1048b8cb041e26dc148de836b21ea885aaae1
Malik <malik96sharfo@hotmail.com>
2018-08-24 11:31:14 +0200

    deleted some useless class in day1.fib

--- src/main/java/day1/fib/FibonacciTask.java
+++ src/main/java/day1/fib/FibonacciTask.java
@@ -10,7 +10,7 @@ Link to exercise: https://docs.google.com/document/d/13f18buNu2WgTocikJ2Quj6Taet
 public class FibonacciTask extends Thread 
 {
     private long tal;
-    List<FibonacciObserver> observers = new ArrayList();
+    private List<FibonacciObserver> observers = new ArrayList();
 
     public void registerFibonacciObserver(FibonacciObserver o) 
     {
@@ -25,10 +25,23 @@ public class FibonacciTask extends Thread
     @Override
     public void run() {
         //Call the Fibonacci method from here
+        fib(tal);
         //long tal = ......
         
         for (FibonacciObserver observer : observers) {
             observer.dataReady(tal);
+        }
+    }
+    
+    private long fib(long n)
+    {
+        if((n == 0) || (n == 1))
+        {
+            return n;
+        }
+        else
+        {
+            return fib(n - 1) + fib(n - 2);
         }
     }
 }
